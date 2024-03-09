37b079a0e0370f327eb7b2b84116fe655c56fac4
SD2 <SD2ayin@users.noreply.github.com>
2024-03-09 06:44:28 +0100

    geometry

---
+++ source/common/geometry.cpp
@@ -0,0 +1,17 @@
+
+#include "geometry.h"
+
+void unitizeGeometry( Geometry *geometry ) {
+    const unsigned numVerts = geometry->vertexPositions.size();
+    vec3_t *vertexPositions = geometry->vertexPositions.data();
+    unsigned vertNum = 0;
+    float maxRadius = 1.0f;
+    while( ++vertNum < numVerts ){
+        maxRadius = wsw::max( maxRadius, VectorLengthFast(vertexPositions[vertNum]) );
+    }
+    const float unitizingFactor = 1/maxRadius;
+    vertNum = 0;
+    while( ++vertNum < numVerts ){
+        VectorScale( vertexPositions[vertNum], unitizingFactor, vertexPositions[vertNum] );
+    }
+}
